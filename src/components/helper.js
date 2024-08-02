449a5cb2861ce634229f58d2c0dfcf88005a9651
artanton <kabletrade@gmail.com>
2024-08-02 12:12:08 +0300

    feature/edit
    creating of modal window
    creating update redux logic
    stylization

--- src/components/helper.js
+++ src/components/helper.js
@@ -3,7 +3,7 @@ export function formatToString(date) {
     const actualDate = new Date(date);
    
    
-    actualDate.setSeconds(0);
+   
     actualDate.setMilliseconds(0);
   
     const year = actualDate.getFullYear();
@@ -13,6 +13,6 @@ export function formatToString(date) {
     const minute = String(actualDate.getMinutes()).padStart(2, '0');
     const second = String(actualDate.getSeconds()).padStart(2, '0');
   
-    const pickedDate = `Date: ${day}-${month}-${year}  Time  ${hour}:${minute}:${second}`;
+    const pickedDate = `${day}-${month}-${year}   ${hour}:${minute}:${second}`;
     return pickedDate;
   }
--- src/components/modal/deleteModal/deleteModalWindow.js
+++ src/components/modal/deleteModal/deleteModalWindow.js
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
-import { deleteTask } from '../../redux/operators';
+import { deleteTask } from '../../../redux/operators';
 import { ModalButton, ModalContent, ModalOverlay } from './modalStyledWindow';
+import { useEffect } from 'react';
 
 export const DeleteConfirmationModal = ({ taskId, onClose }) => {
     const dispatch = useDispatch();
@@ -9,9 +10,28 @@ export const DeleteConfirmationModal = ({ taskId, onClose }) => {
       dispatch(deleteTask(taskId));
       onClose();
     };
+
+    const closeModalOnBackdrop = e => {
+      if (e.target === e.currentTarget) {
+          onClose();
+      }
+    };
+
+    useEffect(() => {
+      const closeModalOnEsc = (e) => {
+        if (e.key === 'Escape') {
+          onClose();
+        }
+      };
+      window.addEventListener('keydown', closeModalOnEsc);
+  
+      return () => {
+        window.removeEventListener('keydown', closeModalOnEsc);
+      };
+    }, [onClose]);
   
     return (
-      <ModalOverlay>
+      <ModalOverlay onClick={closeModalOnBackdrop}>
         <ModalContent>
           <p>Are you sure you want to delete this task?</p>
           <div>
